2e709b8a13a6fbf8088209990a96dfd3a54d2117
Hariprasath M <hariprasath.m@optisolbusiness.com>
2021-11-30 16:02:41 +0530

    latest update

--- demofile/src/test/java/azuredemo1/demo1.java
+++ demofile/src/test/java/azuredemo1/demo1.java
@@ -27,9 +27,9 @@ public class demo1 {
 
 	@Test
 	public void b_Login() throws InterruptedException  {
-	Thread.sleep(5000);
+	Thread.sleep(10000);
 	driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
-	Thread.sleep(5000);
+	Thread.sleep(10000);
 	driver.findElement(By.xpath("//label[contains(text(),'User')]/following::input[1]")).sendKeys("sprint8@yopmail.com");
 	driver.findElement(By.xpath("//label[contains(text(),'User')]/following::input[2]")).sendKeys("Test@123");
 	driver.findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
@@ -37,9 +37,10 @@ public class demo1 {
 	}
 
 	@Test
-	public void c_view_taxcode() {
-		WebDriverWait wait1 = new WebDriverWait(driver,30);
-		WebElement createpost1 =wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Tax code setup')]")));
+	public void c_view_taxcode() throws InterruptedException {
+		//WebDriverWait wait1 = new WebDriverWait(driver,30);
+		//WebElement createpost1 =wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Tax code setup')]")));
+		 Thread.sleep(10000);
 		driver.findElement(By.xpath("//span[contains(text(),'Tax code setup')]")).click();
 		String text1 = driver.findElement(By.xpath("//span[contains(text(),'Tax code setup')]/following::h1[2]")).getText();
 		System.out.println("Heading 1 :-"+text1);
@@ -54,17 +55,19 @@ public class demo1 {
 		String text6 = driver.findElement(By.xpath("//th[contains(text(),'Action')]")).getText();
 		System.out.println(text6);
 		driver.findElement(By.xpath("//button[contains(text(),'Add')]/preceding::input")).sendKeys("ak46%$");
-		WebDriverWait wait2 = new WebDriverWait(driver,30);
-		WebElement createpost2 =wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Add')]")));
+		 
+		//WebDriverWait wait2 = new WebDriverWait(driver,30);
+		//WebElement createpost2 =wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Add')]")));
 	}
 	
 	 @Test
-		public void d_logout()  {
-			WebDriverWait wait9 = new WebDriverWait(driver,30);
-			WebElement createpost9 =wait9.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'sprint eight')]")));
+		public void d_logout() throws InterruptedException  {
+			//WebDriverWait wait9 = new WebDriverWait(driver,30);
+			//WebElement createpost9 =wait9.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'sprint eight')]")));
+		    Thread.sleep(10000);
 			driver.findElement(By.xpath("//span[contains(text(),'sprint eight')]")).click();
 			driver.findElement(By.xpath("//a[contains(text(),'Logout')]")).click();
-			//Thread.sleep(millis);
+			
 			
 			
 	}
